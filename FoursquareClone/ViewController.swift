2a5c8e376c8e0a663f83783ecff3902b6e3ae5f0
Volkan <volkan_goren52@hotmail.com>
2019-12-26 00:10:17 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Volkan on 26.12.2019.
+//  Copyright © 2019 Volkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
